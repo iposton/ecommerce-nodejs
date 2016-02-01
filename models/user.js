7f2789059e820c36daeff256d3ff880b2c4383c6
iposton <ianposton@sbcglobal.net>
2016-02-01 12:15:39 -0800

    Initialize repo

---
+++ models/user.js
@@ -0,0 +1,66 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Schema = mongoose.Schema;
+
+// The User Schema Attributes / Charaterrisitcs / Fields
+
+var UserSchema = new Schema({
+	email: {
+		type: String,
+		unique: true,
+		lowercase: true
+	},
+	password: String,
+
+	profile: {
+		name: {
+			type: String,
+			default: ''
+		},
+		picture: {
+			type: String,
+			default: ''
+		}
+	},
+	address: String,
+
+	history: [{
+		date: Date,
+		paid: {
+			type: Number,
+			default: 0
+		},
+		//item: {
+		//type: Schema.Types.ObjectId,
+
+		//}
+	}]
+});
+
+
+
+// Hash the password before save to database
+UserSchema.pre('save', function(next) {
+	var user = this;
+	if(!user.isModified('password')) return next(); 
+  bcrypt.genSalt(10, function(err, salt) {
+
+  	if(err) return next(err);
+  	bcrypt.hash(user.password, salt, null, function(err, hash) {
+  		if(err) return next(err);
+  		user.password = hash;
+  		next();
+  	});
+
+  });
+
+});
+
+
+// Compare password from database to user input field
+
+UserSchema.methods.comparePassword = function(password) {
+	return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var ejsMate = require('ejs-mate');
+
+var User = require('./models/user')
+
+var app = express();
+
+mongoose.connect('mongodb://admin:password@ds051575.mongolab.com:51575/ecommerce', function(err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('Connected to the data base');
+	}
+})
+
+PORT = 3000;
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+
+app.post('/create-user', function(req, res, next){
+	var user = new User();
+
+	user.profile.name = req.body.name;
+	user.password = req.body.password;
+	user.email = req.body.email;
+
+	user.save(function(err) {
+		if(err) return next(err);
+
+		res.json('Sucessfully created a new user');
+	});
+});
+
+
+app.listen(PORT, function(err) {
+	if(err) throw err; 
+	console.log('Server is running on port '+PORT);
+});
