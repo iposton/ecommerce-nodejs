f5169a3768e9f5e65390db7d7e46f5d8c96a3562
iposton <ianposton@sbcglobal.net>
2016-02-05 10:48:14 -0800

    add login backend and view

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,27 @@
 var router = require('express').Router();
 var User = require('../models/user');
+var passport = require('passport');
+var passportConf = require('../config/passport');
+
+router.get('/login', function(req, res) {
+	if (req.user) return res.redirect('/');
+	res.render('accounts/login', {
+		message: req.flash('loginMessage')
+	});
+});
+
+router.post('/login', passport.authenticate('local-login', {
+	successRedirect: '/profile',
+	failureRedirect: '/login',
+	failureFlash: true
+}));
+
+router.get('/profile', function(req, res, next) {
+	User.findOne({_id: req.user._id}, function(err, user) {
+		if(err) return next(err);
+		res.render('accounts/profile', {user: user});
+	});
+});
 
 router.get('/signup', function(req, res, next) {
 	res.render('accounts/signup', {
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var ejsMate = require('ejs-mate');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
+var MongoStore = require('connect-mongo/es5')(session);
+var passport = require('passport');
 
 var secret = require('./config/secret');
 var User = require('./models/user')
@@ -36,9 +38,15 @@ app.use(cookieParser());
 app.use(session({
 	resave: true,
 	saveUninitialized: true,
-	secret: secret.secretKey
+	secret: secret.secretKey,
+	store: new MongoStore({
+		url: secret.database,
+		autoReconnect: true
+	})
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 var mainRoutes = require('./routes/main');
 var userRoutes = require('./routes/user');
