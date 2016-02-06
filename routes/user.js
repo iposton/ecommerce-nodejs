6154cf7c07c93901ae5341fadda637a51f352b68
iposton <ianposton@sbcglobal.net>
2016-02-05 18:54:12 -0800

    add edit user info

--- routes/user.js
+++ routes/user.js
@@ -60,4 +60,22 @@ router.get('/logout', function(req, res, next) {
 	res.redirect('/');
 });
 
+router.get('/edit-profile', function(req, res, next) {
+	res.render('accounts/edit-profile', {message: req.flash('success')});
+});
+
+router.post('/edit-profile', function(req, res, next) {
+	User.findOne({_id: req.user._id}, function(err, user) {
+		if(err) return next(err);
+		if(req.body.name) user.profile.name = req.body.name;
+		if(req.body.address) user.address = req.body.address;
+
+		user.save(function(err) {
+			if(err) return next(err);
+			req.flash('success', 'Successfully updated your profile.');
+			return res.redirect('/edit-profile');
+		});
+	});
+});
+
 module.exports = router;
